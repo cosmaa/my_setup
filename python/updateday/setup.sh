fcf000f7f461b98a84bab27742b8b54c481a4589
sm <ft-hopper@otto.de>
2022-05-06 12:47:20 +0200

    sm|remove updateday project|updateday
    Co-authored-by: Constanze Maaß <Constanze.maass@otto.de>

--- python/updateday/setup.sh
+++
@@ -1,3 +0,0 @@
-python3 -m venv venv
-venv/bin/python -m pip install --upgrade pip
-venv/bin/python -m pip install -r requirments.txt
--- python/updateday/src/test/test_kotlin.py
+++
@@ -1,6 +0,0 @@
-
-
-
-def test_update(self):
-    print("hello")
-    assert True
